edab359969501df1468e43e34bf87303acca2e76
Pascal Charpentier <59535994+pascalcharp@users.noreply.github.com>
2024-05-18 11:29:04 -0400

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+"""
+Exercice bonus :
+--------------
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+Une fois que vous avez complété les primitives de votre liste simplement chaînée à votre goût, écrivez une fonction qui
+prend une liste contenant 2n éléments, et renverse la deuxième moitié.  Donc la liste suivante :
+[1, 2, 3, 4, 5, 6, 7, 8] devient [1, 2, 3, 4, 8, 7, 6, 5]...
+
+Estimez la complexité de votre algorithme. Estimez aussi la complexité des primitives de la liste
+"""
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def renverser_moitie(l):
+    """Prend une liste l contenant 2n élément et inverse l'ordre des n derniers éléments."""
+    pass
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    maListe = [1, 2, 3, 4, 5, 6, 7, 8]
+    renverser_moitie(maListe)
+    print("Attendu : [1, 2, 3, 4, 8, 7, 6, 5]")
+    print(f"Obtenu  : {maListe}")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
